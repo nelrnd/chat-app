b981a2a33eb9c06b314f02788ef73f3435b72956
Nel <nel.renaudin@gmail.com>
2023-04-13 15:27:04 +0200

    Create ChatRoom basic structure

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,20 @@
+import '../styles/ChatRoom.css';
+
+function ChatRoom() {
+  return (
+    <div className="ChatRoom">
+      <header>Aubrey</header>
+
+      <div>Perfect!</div>
+
+      <div>
+        <form action="#">
+          <input type="text" placeholder="Type in something..." />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ChatRoom;
