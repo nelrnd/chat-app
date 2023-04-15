27ee1b6cca6dadf76c37652611b89d9f7868d5f5
Nel <nel.renaudin@gmail.com>
2023-04-15 11:34:02 +0200

    Redirect home to login if not authenticated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,30 @@
+import { useState, useEffect } from 'react';
+import { auth } from '../firebase';
+import { Navigate } from 'react-router-dom';
+
 import Sidebar from '../components/Sidebar';
 import ChatRoom from '../components/ChatRoom';
 
 import '../styles/Home.css';
 
 function Home() {
-  return (
-    <div className="Home">
-      <Sidebar />
-      <ChatRoom />
-    </div>
-  );
+  const [authenticated, setAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    setAuthenticated(user);
+  }, []);
+
+  if (!authenticated) {
+    return <Navigate to="/login" replace />;
+  } else {
+    return (
+      <div className="Home">
+        <Sidebar />
+        <ChatRoom />
+      </div>
+    );
+  }
 }
 
 export default Home;
