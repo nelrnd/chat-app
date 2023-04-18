e90331cfc0dbb72cd088695110c8f68191e26f89
Nel <nel.renaudin@gmail.com>
2023-04-18 10:27:29 +0200

    Replace :has by something that works on Firefox

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,10 @@
 import '../styles/Message.css';
 
-function Message(props) {
-  const messageClass = props.isSent ? 'sent' : 'received';
-
+function Message({ content, date, isSent }) {
   return (
-    <div className="Message_wrapper">
-      <div className={`Message ${messageClass}`}>{props.text}</div>
-      <div className="Message_date small">{props.date}</div>
+    <div className={`Message_wrapper ${isSent ? 'sent' : 'received'}`}>
+      <div className="Message">{content}</div>
+      <div className="Message_date small">{date}</div>
     </div>
   );
 }
