01b4e278f33fa977db9de9924ef2f5e6c0faae40
Nel <nel.renaudin@gmail.com>
2023-04-18 13:49:54 +0200

    Display message date in correctly formatted format

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,13 @@
 import '../styles/Message.css';
+import { getFormattedDate } from '../utils';
 
 function Message({ content, date, isSent }) {
   return (
     <div className={`Message_wrapper ${isSent ? 'sent' : 'received'}`}>
       <div className="Message">{content}</div>
-      <div className="Message_date small">{date}</div>
+      <div className="Message_date small">
+        {getFormattedDate(date, Date.now())}
+      </div>
     </div>
   );
 }
