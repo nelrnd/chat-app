1e6d17052896648ac46b26427c2711884276887f
Nel <nel.renaudin@gmail.com>
2023-04-18 12:56:58 +0200

    Create and test getFormattedElapsedTime

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -1,4 +1,4 @@
-import { getChatId, getUsersId } from '../utils';
+import { getChatId, getUsersId, getFormattedElapsedTime } from '../utils';
 
 describe('getChatId', () => {
   it('gets chat Id from users Id (1)', () => {
@@ -24,5 +24,44 @@ describe('getUsersId', () => {
     const uid1 = 'jphMUrQdRhgULMsmUEoi9S478FG2';
     const uid2 = 'zPnA9VsqrwWqSpONE7JkyZfhO2y2';
     expect(getUsersId(chatId)).toEqual([uid1, uid2]);
+  });
+});
+
+describe('Get formatted elapsed time', () => {
+  const currentTime = 1681814821848;
+  it('returns 3 years', () => {
+    const time = new Date('03/04/2001').getTime();
+    const result = '22y';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 4 months', () => {
+    const time = new Date('12/17/2022').getTime();
+    const result = '4m';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 2 weeks', () => {
+    const time = new Date('04/04/2023').getTime();
+    const result = '2w';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 6 days', () => {
+    const time = new Date('04/12/2023').getTime();
+    const result = '6d';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 2 hours', () => {
+    const time = 1681807621848;
+    const result = '2h';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 3 minutes', () => {
+    const time = 1681814641848;
+    const result = '3m';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
+  });
+  it('returns 6 seconds', () => {
+    const time = 1681814815848;
+    const result = '6s';
+    expect(getFormattedElapsedTime(time, currentTime)).toBe(result);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,29 @@ export function getChatId(usersId) {
 export function getUsersId(chatId) {
   return chatId.split('-');
 }
+
+export function getFormattedElapsedTime(time, currentTime) {
+  const elapsedTimeInSeconds = Math.floor((currentTime - time) / 1000);
+  const SECS_IN_YEAR = 31540000;
+  const SECS_IN_MONTH = 2628000;
+  const SECS_IN_WEEK = 604800;
+  const SECS_IN_DAY = 86400;
+  const SECS_IN_HOUR = 3600;
+  const SECS_IN_MINUTE = 60;
+
+  if (elapsedTimeInSeconds >= SECS_IN_YEAR) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_YEAR) + 'y';
+  } else if (elapsedTimeInSeconds >= SECS_IN_MONTH) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_MONTH) + 'm';
+  } else if (elapsedTimeInSeconds >= SECS_IN_WEEK) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_WEEK) + 'w';
+  } else if (elapsedTimeInSeconds >= SECS_IN_DAY) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_DAY) + 'd';
+  } else if (elapsedTimeInSeconds >= SECS_IN_HOUR) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_HOUR) + 'h';
+  } else if (elapsedTimeInSeconds >= SECS_IN_MINUTE) {
+    return Math.floor(elapsedTimeInSeconds / SECS_IN_MINUTE) + 'm';
+  } else {
+    return elapsedTimeInSeconds + 's';
+  }
+}
