96645dd252e3bad6f8f8d3a781898072b177ae65
Nel <nel.renaudin@gmail.com>
2023-04-19 10:44:50 +0200

    Sort chats by order of recency in ChatTabs

--- src/components/ChatTabs.js
+++ src/components/ChatTabs.js
@@ -15,17 +15,19 @@ function ChatTabs({ chats, currentChat, handleChatTabClick }) {
   return (
     <div className="ChatTabs">
       {chats &&
-        chats.map((chat) => (
-          <ChatTab
-            key={chat.id}
-            name={chat.name}
-            profileURL={chat.profileURL}
-            lastMessage={chat.lastMessage}
-            isActive={currentChat === chat.id}
-            currentTime={currentTime}
-            handleClick={() => handleChatTabClick(chat.id)}
-          />
-        ))}
+        chats
+          .sort((a, b) => b.lastMessage.date - a.lastMessage.date)
+          .map((chat) => (
+            <ChatTab
+              key={chat.id}
+              name={chat.name}
+              profileURL={chat.profileURL}
+              lastMessage={chat.lastMessage}
+              isActive={currentChat === chat.id}
+              currentTime={currentTime}
+              handleClick={() => handleChatTabClick(chat.id)}
+            />
+          ))}
     </div>
   );
 }
