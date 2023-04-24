e730c4b62a347f4fe6fb7db2751f9667d354f6cc
Nel <nel.renaudin@gmail.com>
2023-04-24 10:58:15 +0200

    Create EditProfileModal component

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -8,11 +8,14 @@ import { ReactComponent as EditIcon } from '../assets/icons/edit.svg';
 import { ReactComponent as CopyIcon } from '../assets/icons/clipboard.svg';
 import Layout from '../components/Layout';
 import { signOut } from 'firebase/auth';
+import Modal from '../components/Modal';
+import { useState } from 'react';
 
 const logout = () => signOut(auth);
 
 function Settings() {
   const [user, loading] = useAuthState(auth);
+  const [showEditProfileModal, setShowEditProfileModal] = useState(false);
 
   if (!user && !loading) {
     return <Navigate to="/login" replace />;
@@ -35,7 +38,10 @@ function Settings() {
               </div>
             </div>
 
-            <button className="secondary small">
+            <button
+              className="secondary small"
+              onClick={() => setShowEditProfileModal(true)}
+            >
               <EditIcon />
               EDIT
             </button>
@@ -57,9 +63,95 @@ function Settings() {
             <button onClick={logout}>Logout</button>
           </section>
         </div>
+
+        <EditProfileModal
+          show={showEditProfileModal}
+          profileURL={user.photoURL}
+          name={user.displayName}
+          handleClose={() => setShowEditProfileModal(false)}
+        />
       </Layout>
     );
   }
+}
+
+function EditProfileModal({ show, name, profileURL, handleClose }) {
+  const [newProfileURL, setNewProfileURL] = useState(profileURL);
+  const [newName, setNewName] = useState(name);
+
+  const handleNameChange = (e) => setNewName(e.target.value);
+
+  const handleProfileURLChange = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const profileURL = URL.createObjectURL(file);
+      setNewProfileURL(profileURL);
+    }
+  };
+
+  const handleRemoveProfileURL = () => setNewProfileURL(null);
+
+  const clearFields = () => {
+    setNewProfileURL(profileURL);
+    setNewName(name);
+  };
+
+  return (
+    <Modal show={show}>
+      <h2>Edit profile</h2>
+
+      <div className="flex-row gap32 align-items">
+        <Avatar imageURL={newProfileURL} size="large" />
+
+        <div className="flex-row gap16">
+          <label htmlFor="new-profile-pic" className="button small">
+            Upload
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            name="new-profile-pic"
+            id="new-profile-pic"
+            onChange={handleProfileURLChange}
+            className="hidden"
+          />
+          {newProfileURL && (
+            <button
+              className="secondary small"
+              onClick={handleRemoveProfileURL}
+            >
+              Remove
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor={name}>Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="outline"
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+
+      <div className="row-left">
+        <button
+          onClick={() => {
+            handleClose();
+            clearFields();
+          }}
+          className="secondary small"
+        >
+          Cancel
+        </button>
+        <button className="small">Save</button>
+      </div>
+    </Modal>
+  );
 }
 
 export default Settings;
