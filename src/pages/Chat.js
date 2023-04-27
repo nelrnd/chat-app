96453e944f0502a520c563a2cee4dc45e29dfaa8
Nel <nel.renaudin@gmail.com>
2023-04-27 08:26:50 +0200

    Handle redirect restricted chat to home page

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import { auth } from '../firebase';
 import ChatRoom from '../components/ChatRoom';
+import { getUserIds } from '../utils';
 
 function Chat() {
   const params = useParams();
@@ -13,6 +14,10 @@ function Chat() {
   const [user] = useAuthState(auth);
 
   if (user) {
+    if (!getUserIds(chatId).includes(user.uid)) {
+      return <Navigate to="/" replace />;
+    }
+
     return (
       <Layout>
         <Sidebar userId={user.uid} currentChat={chatId} />
